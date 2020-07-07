48dbf72adce0d5ed8e7fd5467ff79384fd67b06f
Stephan Guthier <Stephan.Guthier@fiduciagad.de>
2020-07-07 11:57:29 +0200

    comment

--- org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -2,6 +2,9 @@
  * Copyright (C) 2010, Matthias Sohn <matthias.sohn@sap.com>
  * Copyright (C) 2010, Stefan Lay <stefan.lay@sap.com>
  *
+ * HALLÖLE
+ *
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
